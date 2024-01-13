6a8ad6e28aec970fa9be92d22661e30f453a244c
Ivan Leon <leonsanp@gmail.com>
2024-01-13 17:59:03 -0500

    hero progress

--- src/Components/SwiperBasic/index.jsx
+++ src/Components/SwiperBasic/index.jsx
@@ -17,15 +17,15 @@ export default function SwiperBasic({ pryceForm }) {
         <div className="min-w-[60%] w-[90%]   md:w-full md:h-[383px] rounded-xl md:rounded-none mx-auto md:flex md:justify-center 
         
         
-        border border-blue-500
+        
         "> 
         {/* bg-image bg-cover bg-center */}
 
         {/* container principal  */}
 
-            <div className="flex flex-col w-full md:w-[45%]  my-4 border border-red-900  ">
+            <div className="flex flex-col w-full md:w-[45%]  my-4  ">
 
-                <h2 className="mb-2 py-2 md:py-1.5 w-[80%] md:w-[70%] text-center text-[20px] md:text-2xl tracking-widest font-bold  rounded-r-md  italic text-white
+                <h2 className="mb-2 py-2 md:py-1.5 w-[80%] md:w-[70%] text-center text-[20px] md:text-3xl tracking-widest font-bold  rounded-l-lg  italic text-yellow-100
                 
                 bg-gradient-to-r from-cyan-800 to-slate-50
                 ">
@@ -46,40 +46,44 @@ export default function SwiperBasic({ pryceForm }) {
             </div>
 
             <div className="mt-4 text-center md:text-right md:my-4 md:mr-15 rounded-r-lg 
-            bg-gradient-to-l from-black to-slate-50  border border-yellow-600            
+            bg-gradient-to-l from-cyan-800 to-slate-50              
+            border border-red-800
             ">
 
-                <h2 className="font-bold text-sm md:text-base md:text-white md:pr-6 md:mt-2">
+                <h2 className="font-bold text-sm md:text-base md:text-yellow-100 text-right mr-12  md:mt-2">
                     Seguro vehicular al 100%
                 </h2>
 
 
-                <div className="w-[80%] mx-auto md:mr-2 md:pr-6 my-2  flex flex-col ">
-                    <p className=" flex items-start text-xs pr-14 md:text-xl   italic md:text-white">
+                <div className="w-[80%] mx-auto md:mr-2 md:mr-12 my-2  flex flex-col ">
+                    <p className=" flex justify-end text-xs md:text-xl italic md:text-yellow-100">
                         Desde
                     </p>
-                    <h2 className="text-4xl md:text-7xl font-bold md:my-2 drop-shadow-lg tracking-wider">
+                    <h2 className="text-5xl md:text-7xl font-bold md:my-2 drop-shadow-xl tracking-widest text-teal-100 ">
                         ${pryceForm.Basic}
                     </h2>
-                    <h2 className="flex items-start text-xs pl-14 md:pl-0 md:text-xl italic">
+                    {/* <h2 className="flex items-start text-xs pl-14 md:pl-0 md:text-xl italic">
                         Por mes
-                    </h2>
+                    </h2> */}
                 </div>
 
-                <div className="flex gap-4 pl-0 pr-5 mt-4  items-center text-sm md:text-lg text-left ">
-                    <img className="w-4 h-4" src={star} alt="" />
+                <div className="border border-green-800  w-2/4  ">
+                <div className=" mt-2  text-sm md:text-lg text-right text-wrap text-white ">
+                    {/* <img className="w-4 h-4 " src={star} alt="" /> */}
                     <p>
                         Asistencia las 24 h para su vehículo, sin preocupaciones de
                         más.
                     </p>
                 </div>
 
-                <div className="flex gap-4 pl-0 pr-5 mt-6  items-center text-sm md:text-lg text-left ">
+                <div className="flex gap-4 pl-0 pr-5 mt-6  items-center text-sm md:text-lg text-right text-white text-wrap  ">
                     <img className="w-4 h-4 " src={star} alt="" />
                     <p>
-                        Revisión técnica mensualmente para el seguro de tu auto y el
+                        Revisión técnica mensualmente</p> 
+                        <p>para el seguro de tu auto y el
                         tuyo.
                     </p>
+                </div>
                 </div>
 
                 <div className=" flex justify-center mt-5 ">
