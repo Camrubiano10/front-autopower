97aaaca87e1e18b3dea95e2a085347af41617f9e
Ivan Leon <leonsanp@gmail.com>
2024-01-22 16:58:52 -0500

    BuyService hoja 4.1

--- src/Components/PaymentConfirmation/index.jsx
+++ src/Components/PaymentConfirmation/index.jsx
@@ -6,6 +6,7 @@ import star from "../../assets/icons/star.png";
 import { peticionesBack } from "../../Helpers/fetch"
 
 export default function PaymentConfirmation({ valoresForms }) {
+
   const saveTable=async()=>{
     
     let dataClient = {
@@ -20,16 +21,13 @@ export default function PaymentConfirmation({ valoresForms }) {
       license_plate: valoresForms.placa,
       service: valoresForms.service
     }
-
     
-
-
     const client = await peticionesBack('client/', 'POST', valoresForms)
     console.log(client)
   }
 
   return (
-    <FormWindow className={"border flex flex-col pb-14 md:h-[75.2vh]"}>
+    <FormWindow >
       <div className="bg-darkblue-select py-6 flex justify-center rounded-t-2xl">
         <img className="md:w-50 w-25 p-4" src={like} />
       </div>
@@ -42,7 +40,7 @@ export default function PaymentConfirmation({ valoresForms }) {
           id="nameCongratulation"
           className="text-lg font-bold py-2 uppercase"
         >
-          {valoresForms.nombre}
+          {valoresForms.name}
         </h3>
 
         <p className="pb-4">
