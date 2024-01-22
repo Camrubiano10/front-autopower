9fb99398ccd5cab3ad8a685eb416226b07dafcf9
Ivan Leon <leonsanp@gmail.com>
2024-01-22 12:37:05 -0500

    BuyService 1-2 + responsive completo

--- src/Components/ContainerBill/index.jsx
+++ src/Components/ContainerBill/index.jsx
@@ -28,62 +28,62 @@ export default function ContainerBill({ setActiveStep }) {
       RESUMEN PLAN {planSeleccionado.plan}
       </h2>
 
-    <div className="flex justify-between  ">
+    <div className="flex flex-col md:flex-row justify-between border border-white ">
     {/* contenedor 1  */}
-    <div className=" w-1/2 ">
+    <div className=" md:w-1/2 ">
 
 
-      <div className="flex mb-4 justify-between">
-        <div className="ml-8 mt-3 font-bold text-cyan-700 uppercase tracking-wider ">
+      <div className="flex mb-4 justify-between ">
+        <div className="ml-8 mt-4 md:mt-3 font-bold text-cyan-700 uppercase tracking-wider ">
             <p className=" ">Vigencia:</p>
         </div>
-        <div className="mt-3  mr-8 font-semibold  uppercase ">
+        <div className="mt-4 md:mt-3  mr-8 font-semibold  uppercase ">
             <p className=" ">Mensual</p>
         </div>        
       </div>
 
         <div className="flex mb-4 justify-between">
-          <div className="ml-8 mt-3 font-bold text-cyan-700 uppercase tracking-wider">
+          <div className="ml-8 mt-4 md:mt-3 font-bold text-cyan-700 uppercase tracking-wider">
             <p className=" ">Fecha de Inicio:</p>
           </div>
-          <div className="mt-3  mr-8 font-semibold  uppercase">
+          <div className="mt-4 md:mt-3  mr-8 font-semibold  uppercase">
             <p className=" ">{today.toLocaleDateString()}</p>
           </div>
         </div>
 
         <div className="flex mb-4 justify-between">
-          <div className="ml-8 mt-3 font-bold text-cyan-700 uppercase tracking-wider ">
+          <div className="ml-8 mt-4 md:mt-3 font-bold text-cyan-700 uppercase tracking-wider ">
             <p className=" ">Fecha fin:</p>
           </div>
-          <div className="mt-3  mr-8 font-semibold  uppercase ">
+          <div className="mt-4 md:mt-3  mr-8 font-semibold  uppercase ">
             <p className="">{datePeriodo.toLocaleDateString()}</p>
           </div>
         </div>
 
         <div className="flex mb-4 mt-5 justify-between">
-          <div className="ml-8 mt-3 font-bold text-cyan-700 uppercase tracking-wider ">
+          <div className="ml-8 mt-4 md:mt-3 font-bold text-cyan-700 uppercase tracking-wider ">
             <p className="">Tarifa Mensual:</p>
           </div>
-          <div className="mt-3  mr-8 font-semibold  uppercase">
+          <div className="mt-4 md:mt-3  mr-8 font-semibold  uppercase">
             <p className="">${planSeleccionado.precio}</p>
           </div>
         </div>
 
         <div className="flex mb-4 justify-between">
-          <div className="ml-8 mt-3 font-bold text-cyan-700 uppercase tracking-wider ">
+          <div className="ml-8 mt-4 md:mt-3 font-bold text-cyan-700 uppercase tracking-wider ">
             <p className="">IGV:</p>
           </div>
-          <div className="mt-3  mr-8 font-semibold  uppercase">
+          <div className="mt-4 md:mt-3  mr-8 font-semibold  uppercase">
             <p className="">18%</p>
           </div>
         </div>
 
 
         <div className="flex mb-4 justify-between">
-          <div className="ml-8 mt-3 font-bold text-cyan-700 uppercase tracking-wider ">
+          <div className="ml-8 mt-4 md:mt-3 font-bold text-cyan-700 uppercase tracking-wider ">
             <p className="">Pago total:</p>
           </div>
-          <div className="mt-3  mr-8 font-semibold  uppercase ">
+          <div className="mt-4 md:mt-3  mr-8 font-semibold  uppercase ">
             <p className="">${priceTotal}</p>
           </div>
         </div>
@@ -91,13 +91,13 @@ export default function ContainerBill({ setActiveStep }) {
       </div>
 
       {/* contenedor 2 */}
-    <div className=" w-1/2 ">
+    <div className="mt-5 md:mt-0 md:w-1/2 ">
 
-      <div className="h-[280px] border-4 border-cyan-100 bg-image-login bg-cover mx-8 rounded-lg mt-3 ">
+      <div className="md:h-[280px] border-4 border-cyan-100 bg-image-login bg-cover mx-8 rounded-lg md:mt-3 invisible md:visible">
 
       </div>
 
-          <div className="text-center mt-3 border-2 border-white h-1/7">
+          <div className=" text-center md:mt-3 border-2 border-white h-1/7">
             <p className="text-blue-700">
               <a href="#" className="text-xs">
                 <span>
@@ -111,7 +111,7 @@ export default function ContainerBill({ setActiveStep }) {
 
   </div>
 
-        <div className="flex justify-center mt-7">
+        <div className="flex justify-center mt-10 md:mt-7">
           <Button type="submit" text="Siguiente" variant="primary" />
         </div>
       </form>
